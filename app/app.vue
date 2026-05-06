77dab0e2d850aae5ac5445effe26fc2176ba5ec7
yungNATE <nathan.martinigol@gmail.com>
2026-05-06 23:51:02 +0200

    Test du scroll horizontal

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,65 @@
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { mapVerticalToHorizontalScroll } from "./utils/horizontalScroll";
+
+const scrollWrapper = ref(null);
+let dispose: (() => void) | null = null;
+
+onMounted(() => {
+  if (!scrollWrapper.value) return;
+  dispose = mapVerticalToHorizontalScroll(scrollWrapper.value, {
+    duration: 0.8,
+    speedMultiplier: 1,
+  });
+});
+
+onBeforeUnmount(() => {
+  dispose?.();
+});
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div ref="scrollWrapper" class="horizontal-scroll-wrapper">
+    <div>item 1</div>
+    <div>item 2</div>
+    <div>item 3</div>
+    <div>item 4</div>
+    <div>item 5</div>
   </div>
 </template>
+
+<style lang="scss">
+.horizontal-scroll-wrapper {
+  height: 100%;
+  display: flex;
+  overflow-x: scroll;
+
+  > div {
+    color: white;
+    height: 100%;
+    flex: 0 0 auto;
+
+    &:nth-child(1) {
+      background-color: #474aff;
+      width: calc(100vw + 10px);
+    }
+    &:nth-child(2) {
+      background-color: #ff0073;
+      width: calc(100vw + 300px);
+    }
+    &:nth-child(3) {
+      background-color: #ff3737;
+      width: calc(100vw + 200px);
+    }
+    &:nth-child(4) {
+      background-color: #ff7700;
+      width: calc(100vw + 10px);
+    }
+    &:nth-child(5) {
+      background-color: #f2e8e5;
+      color: black;
+      width: 100vw;
+    }
+  }
+}
+</style>
---
+++ app/utils/horizontalScroll.ts
@@ -0,0 +1,46 @@
+import gsap from "gsap";
+
+export type HorizontalScrollOptions = {
+  duration?: number;
+  ease?: string;
+  speedMultiplier?: number;
+};
+
+/**
+ * Map vertical wheel events to horizontal scroll on the provided element.
+ * Returns a disposer function to remove the listener and stop GSAP tweens.
+ */
+export function mapVerticalToHorizontalScroll(
+  el: HTMLElement,
+  opts: HorizontalScrollOptions = {},
+) {
+  const { duration = 0.8, ease = "power2.out", speedMultiplier = 1 } = opts;
+
+  const onWheel = (e: WheelEvent) => {
+    e.preventDefault();
+
+    const maxScroll = el.scrollWidth - el.clientWidth;
+    const currentScroll = el.scrollLeft;
+
+    const screenRatio = window.innerWidth / window.innerHeight;
+    const speedRatioFactor = Math.min(Math.max(1, screenRatio), 2);
+
+    const delta = e.deltaY * speedMultiplier * speedRatioFactor;
+    const newScroll = Math.max(0, Math.min(currentScroll + delta, maxScroll));
+
+    gsap.to(el, {
+      scrollLeft: newScroll,
+      duration,
+      ease,
+    });
+  };
+
+  el.addEventListener("wheel", onWheel, { passive: false });
+
+  return () => {
+    el.removeEventListener("wheel", onWheel);
+    gsap.killTweensOf(el);
+  };
+}
+
+export default mapVerticalToHorizontalScroll;
