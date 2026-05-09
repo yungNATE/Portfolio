b5f30936d29026e879db9cde90d2cd7731dfad0c
yungNATE <nathan.martinigol@gmail.com>
2026-05-09 19:04:58 +0200

    debug dans nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  debug: true,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["@/assets/css/reset.css", "@/assets/css/global.scss"],
